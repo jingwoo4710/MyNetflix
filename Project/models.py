07c8ada86d521dd439e4a7bfa248cf63e95ebb39
jingwoo4710 <jaewoo4710@gmail.com>
2020-12-03 01:05:44 +0900

    make it better

--- Project/models.py
+++ Project/models.py
@@ -32,7 +32,7 @@ class Lists(db.Model):
     __tablename__ = "Lists"
     id = db.Column(db.Integer, primary_key = True)
     user = db.Column(db.String, db.ForeignKey("Users.id"))
-    movie = db.Column(db.String, db.ForeignKey("Movies.title"))
+    movie = db.Column(db.String, db.ForeignKey("Movies.id"))
 
     users = db.relationship("Users", foreign_keys=user)
     movies = db.relationship("Movies", foreign_keys=movie)
