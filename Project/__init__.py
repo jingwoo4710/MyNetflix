d0eaae7fbdae3d49dbb115c01dbaa7cdefbe86ec
jingwoo4710 <jaewoo4710@gmail.com>
2020-12-04 15:20:20 +0900

    make it better

--- Project/__init__.py
+++ Project/__init__.py
@@ -10,8 +10,8 @@ en = EmbeddingClient(host='54.180.124.154', port=8989)
 
 def create_app():
     app = Flask(__name__)
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///netflix.sqlite3'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jzldammovjblxe:3786b05155a588f1097c298f227121cbb08740f95e6dca5df61498e7f9138d86@ec2-75-101-212-64.compute-1.amazonaws.com:5432/d55tm2dunhdufa'
     # "postgres://jzldammovjblxe:3786b05155a588f1097c298f227121cbb08740f95e6dca5df61498e7f9138d86@ec2-75-101-212-64.compute-1.amazonaws.com:5432/d55tm2dunhdufa"
     # "sqlite:///netflix.sqlite3"
     app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True 
